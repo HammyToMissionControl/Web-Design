61a3d3d7f302c3da3dbeb5d00f8043985fae9633
HammyToMissionControl <riverharmer08@gmail.com>
2026-04-21 00:13:39 +0100

    Api key

--- Auth/Controllers/CheckoutController.cs
+++ Auth/Controllers/CheckoutController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Stripe;
+using Stripe.Checkout;
 
 namespace Auth.Controllers
 {
@@ -17,10 +19,13 @@ namespace Auth.Controllers
             _userManager = userManager;
         }
 
+
+
+        [HttpPost]
         public async Task<IActionResult> Checkout()
         {
             var user = await _userManager.GetUserAsync(User);
-            
+
             if (user == null)
             {
                 return View("Error");
@@ -31,8 +36,52 @@ namespace Auth.Controllers
                 .ThenInclude(i => i.Product)
                 .FirstOrDefault(u => u.UserId == user.Id);
 
+            if (basket == null || !basket.Products.Any())
+            {
+                return RedirectToAction("Index", "Basket");
+            }
 
+            var LineItems = basket.Products.Select(item => new SessionLineItemOptions
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    Currency = "gbp",
+                    UnitAmount = (long)(item.Product.Price * 100),
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = item.Product.Name,
+                    }
+                },
+                Quantity = item.Quantity
+            }).ToList();
+
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = LineItems,
+                Mode = "payment",
+                SuccessUrl = "https://localhost:7262/Checkout/Success",
+                CancelUrl = "https://localhost:7262/Checkout/Cancel",
+            };
+
+            var service = new SessionService();
+            var session = await service.CreateAsync(options);
+            return Redirect(session.Url);
+        }
+
+
+        }
+
+        public IActionResult Success()
+        {
             return View();
         }
+
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+
+       
     }
 }
